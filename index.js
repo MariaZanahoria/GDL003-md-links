3f534f10ea6646d45b47ba38cc24890a53641338
MariaZanahoria <m.iselacarrillo.p@gmail.com>
2019-08-15 18:55:30 -0500

    test y funcion de buscar archivo MD en un Path

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-module.exports = () => {
-  // ...
-};
+const path = require('path')
+
+module.exports = filePath => path.extname(filePath)=== '.md';
+
+export const miFunction = () => {
+  
+}
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -3,8 +3,10 @@ const mdLinks = require('../');
 
 describe('mdLinks', () => {
 
-  it('should...', () => {
-    console.log('FIX ME!');
+  it('should return be true for a valid .md file', () => {
+    expect(mdLinks('../README.md')).toBe(true);
+   // console.log('');
   });
-
+  it('should return be false for a valid .md file', () => {
+    expect(mdLinks('../index.js')).toBe(false);
 });
