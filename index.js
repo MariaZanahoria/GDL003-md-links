719b02f3b382de1f192bdb255e252c7dc0c9cd21
MariaZanahoria <m.iselacarrillo.p@gmail.com>
2019-08-27 18:34:24 -0500

    funcion de validar el path

--- index.js
+++ index.js
@@ -3,29 +3,29 @@ const path = require('path');
 const file = require('fs');
 const route = process.argv[2];
 
-mdLinks.mdSearch = route => path.extname(route) === '.md';
-console.log('¿Existen archivos Markdown? ' + route); 
+mdLinks.correctFile = route => file.stat(route, (err)=>{
+    if (err === null) {
+        console.log("El archivo es valido");
+    } else if (err.code == 'ENOENT') {
+        console.log ("El archivo no existe");
+    };
+});
 
-mdLinks.foundUrls = (route, callback) => {
-    file.readFile(route, 'utf8', (error, mdInfo) => {
+mdLinks.mdSearch = route => path.extname(route) === '.md';
+console.log('¿Existen archivos Markdown?: ' + mdLinks.mdSearch(route));
+
+mdLinks.foundUrls = route => file.readFile(route, 'utf8', (error,mdInfo) => {
         if (error) {
             throw error;
         }
         const regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g;
-        const found = mdInfo.match(regex);
-        const urls = [];
-        for (let i = 0; i < found.length; i++) {
-            urls.push(found[i]);
-        console.log(mdLinks.foundUrls);
-        };
-        callback(mdInfo);
-});
-};
+        const linksFound = mdInfo.match(regex);
+        console.log('Estos son los link que se localizan en tu archivo md: \n ', linksFound);
+    });
 
-
-
-
-
-
+mdLinks.correctFile(route);
+mdLinks.mdSearch(route);
+mdLinks.foundUrls(route);
 
 module.exports = mdLinks;
+
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,20 +1,24 @@
 const mdLinks = require('../');
 
 describe('mdLinks', () => {
+
+  /*it('should return be true for a valid file', () => {
+    expect(mdLinks.correctFile('../README.md')).toBe(true);
+  });
+  it('should return be false for a invalid file', () => {
+    expect(mdLinks.correctFile('../wiwuwi')).toBe(false);
+  });*/
+
   it('should return be true for a valid .md file', () => {
     expect(mdLinks.mdSearch('../README.md')).toBe(true);
   });
-  it('should return be false for a valid .md file', () => {
+  it('should return be false for a invalid .md file', () => {
     expect(mdLinks.mdSearch('../index.js')).toBe(false);
-})
-});
+  });
 
-describe('mdLinks', () => {
   it('should read and impress the links on the .md file', () => {
-    mdLinks.foundUrls('route',(mdInfo)=>{
-      expect(mdinfo).toBe('Estos son los links que contine el archivo .md');
+    mdLinks.foundUrls("route", (mdInfo) => {
+      expect(mdInfo).toBe(linksFound)
     });
   });
 });
-
-
